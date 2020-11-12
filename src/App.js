cf19278211038b4b674555dd5c2e76619c9c6b9d
Manisrinivasa1999 <manisrinivasa1999@gmail.com>
2020-11-12 18:14:20 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -41,11 +41,11 @@ function App() {
 
   const fetchImages = (count = 10) => {
     const apiRoot = 'https://api.unsplash.com';
-    // const accessKey = process.env.REACT_APP_ACCESSKEY;
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
 
     axios
       .get(
-        `${apiRoot}/photos/random?client_id=ribDIB12qExVwBVZXk392PWEqRlkGdKTwenGefJ91o4&count=${count}`
+        `${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`
       )
       .then((res) => {
         setImage([...images, ...res.data]);
